f4e9c892ee2de8f2971ff05de7c638ec62e11115
nayeongdev <nayeongkim.dev@gmail.com>
2025-07-05 21:49:50 +0900

    feat: Loading 컴포넌트 추가

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import React, { useState } from "react";
 
+import Loader from "@/components/loader/Loader";
 import LogoPath from "@/assets/colorful.svg";
 import { useRouter } from "next/navigation";
 
@@ -25,24 +26,29 @@ const LoginClient = () => {
   };
 
   return (
-    <section className={styles.page}>
-      <div className={styles.container}>
-        <h1 className={styles.logo}>
-          <Image src={LogoPath} alt="로고 이미지" />
-        </h1>
+    <>
+      {isLoading && <Loader />}
+      <section className={styles.page}>
+        <div className={styles.container}>
+          <h1 className={styles.logo}>
+            <Image src={LogoPath} alt="로고 이미지" />
+          </h1>
 
-        <form className={styles.form} onSubmit={loginUser}>
-          {/* Input */}
-          <div className={styles.group}>{/* 자동 로그인/비밀번호 수정 */}</div>
+          <form className={styles.form} onSubmit={loginUser}>
+            {/* Input */}
+            <div className={styles.group}>
+              {/* 자동 로그인/비밀번호 수정 */}
+            </div>
 
-          <div className={styles.buttonGroup}>
-            {/* Button */}
+            <div className={styles.buttonGroup}>
+              {/* Button */}
 
-            <div>{/* Button */}</div>
-          </div>
-        </form>
-      </div>
-    </section>
+              <div>{/* Button */}</div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const Loader = () => {
+import { FadeLoader } from "react-spinners";
+
+import styles from "./Loader.module.scss";
+
+const Loader = ({ basic }) => {
+  if (basic) {
+    return (
+      <div className={styles.basicWrapper}>
+        <FadeLoader color="grey" speedMultiplier={0.75} />
+      </div>
+    );
+  }
+
   return (
-    <div>Loader</div>
-  )
-}
+    <div className={styles.wrapper}>
+      <div className={styles.loader}>
+        <FadeLoader color="grey" speedMultiplier={0.75} />
+      </div>
+    </div>
+  );
+};
 
-export default Loader 
+export default Loader;
