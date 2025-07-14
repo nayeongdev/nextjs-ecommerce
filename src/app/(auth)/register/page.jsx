ae7ad27720101041f4c89bca6cc185dafd3feb39
nayeongdev <nayeongkim.dev@gmail.com>
2025-07-14 11:49:22 +0900

    feat: 회원가입 페이지 UI 추가
    - 회원가입 폼 컴포넌트 추가(이메일, 비밀번호, 비밀번호 확인 입력 필드)
    - 회원가입 버튼 및 로그인 페이지 이동 버튼
    - 기존 Auth.module.scss 스타일 재사용

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import RegisterClient from "./RegisterClient";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  return <RegisterClient />;
+};
 
-export default Register
+export default Register;
