5339bcbfaa5f04071caab2bcf410aeedbc801228
nayeongdev <nayeongkim.dev@gmail.com>
2025-08-13 15:23:56 +0900

    feat: 상품 아이템 컴포넌트 UI 구현

--- src/components/product/ProductItem/ProductItem.jsx
+++ src/components/product/ProductItem/ProductItem.jsx
@@ -2,13 +2,42 @@
 
 import React from "react";
 import styles from "./ProductItem.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+import priceFormat from "@/utils/priceFormat";
+import { Rating } from 'react-simple-star-rating'
+import rocketBage from '@/assets/badge-rocket.svg'
 
-const ProductItem = () => {
+const ProductItem = ({ id, name, imageUrl, price }) => {
+
+  const shortenText = (text, maxLength = 10) => {
+    if (text.length <= maxLength) return text;
+    return text.slice(0, maxLength) + "...";
+  };
+
   return (
-    <div>
-      <h2>Product Item</h2>
+    <div className={styles.grid}>
+      <Link href={`/product-details/${id}`}>
+        <div className={styles.img}>
+          <Image src={imageUrl} alt={name} width={265} height={265} />
+        </div>
+      </Link>
+
+      <div className={styles.content}>
+        <div className={styles.details}>
+          <p>{shortenText(name)}</p>
+          <em>
+            <strong style={{ color: "#cb1400" }}>{priceFormat(price)}</strong>원{" "}
+            <Image src={rocketBage} alt='로켓배송'></Image>
+          </em>
+          <div className={styles.rating}>
+            <Rating readonly initialValue={4} size={16} />
+            <span className={styles.ratingCount}>({3})</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default ProductItem; 
+export default ProductItem;
